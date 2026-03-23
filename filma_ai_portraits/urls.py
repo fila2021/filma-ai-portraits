a17f37f4d9bafced093ccb95500c428099a178a6
fila2021 <timtey2015@gmail.com>
2026-03-23 22:09:37 +0000

    Add login and logout routes using Django auth

--- filma_ai_portraits/urls.py
+++ filma_ai_portraits/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views as auth_views
 from home.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
-    path('accounts/', include('accounts.urls')),
+    path('accounts/signup/', include('accounts.urls')),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
