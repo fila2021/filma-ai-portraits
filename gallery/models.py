965ca944911db2cc8f69014f2318e3d67f95066c
fila2021 <timtey2015@gmail.com>
2026-03-23 22:32:03 +0000

    Create gallery image model

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class GalleryImage(models.Model):
+    title = models.CharField(max_length=200)
+    image_url = models.URLField()
+    category = models.CharField(max_length=100, blank=True)
+    caption = models.TextField(blank=True)
+    is_featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
