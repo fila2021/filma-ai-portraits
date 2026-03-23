e8567dc7fc0a382e71d00a05498b03750507cad9
fila2021 <timtey2015@gmail.com>
2026-03-23 22:49:24 +0000

    Create product model for shop app

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField()
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
