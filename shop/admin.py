43cc55e20b6631bbea38cbe49601165a75be2961
fila2021 <timtey2015@gmail.com>
2026-03-23 22:49:45 +0000

    Register product model in admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'is_active', 'created_at')
+    list_filter = ('is_active',)
+    search_fields = ('title', 'description')
+    prepopulated_fields = {'slug': ('title',)}
