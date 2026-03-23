c743139401105f9291e65fa072ceee45e85e9e11
fila2021 <timtey2015@gmail.com>
2026-03-23 22:51:35 +0000

    Include shop routes in project URLs

--- filma_ai_portraits/urls.py
+++ filma_ai_portraits/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('gallery/', include('gallery.urls')),
+    path('shop/', include('shop.urls')),
 ]
