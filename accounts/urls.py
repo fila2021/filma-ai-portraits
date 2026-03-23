460e29ea8dbef166704959c63ee7082e1aab9756
fila2021 <timtey2015@gmail.com>
2026-03-23 22:19:55 +0000

    Add profile detail and edit profile routes

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import signup
+from .views import edit_profile, profile_detail, signup
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
+    path('profile/', profile_detail, name='profile'),
+    path('profile/edit/', edit_profile, name='edit_profile'),
 ]
