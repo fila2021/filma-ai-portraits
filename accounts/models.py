50c8db850b4155b6e629d472533ff65edeba5011
fila2021 <timtey2015@gmail.com>
2026-03-23 22:17:39 +0000

    Create profile model for user accounts

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    display_name = models.CharField(max_length=100)
+    bio = models.TextField(blank=True)
+    instagram_handle = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.display_name or self.user.username
