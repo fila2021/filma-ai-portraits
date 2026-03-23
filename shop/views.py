f21490fe658c6f0523a487db0bef3b9cbc43c06b
fila2021 <timtey2015@gmail.com>
2026-03-23 22:50:22 +0000

    Create product list and detail views

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Product
 
-# Create your views here.
+
+def product_list(request):
+    products = Product.objects.filter(is_active=True).order_by('-created_at')
+    return render(request, 'shop/product_list.html', {'products': products})
+
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug, is_active=True)
+    return render(request, 'shop/product_detail.html', {'product': product})
