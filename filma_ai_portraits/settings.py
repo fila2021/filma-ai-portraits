cb60badb8e0ec162675e516f223116ade76314d7
fila2021 <timtey2015@gmail.com>
2026-03-23 21:57:16 +0000

    Configure templates and static files

--- filma_ai_portraits/settings.py
+++ filma_ai_portraits/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'filma_ai_portraits.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
