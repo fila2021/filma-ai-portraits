b2ea43153c92f362d2dd3bd18070ea3d06f3e102
fila2021 <timtey2015@gmail.com>
2026-03-23 22:34:36 +0000

    Include gallery routes in project URLs

--- filma_ai_portraits/urls.py
+++ filma_ai_portraits/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('gallery/', include('gallery.urls')),
 ]
