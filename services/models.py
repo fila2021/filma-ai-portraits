f5aa56e879a25743072c66212412621b731293cf
fila2021 <timtey2015@gmail.com>
2026-03-23 22:56:50 +0000

    Create service package model

--- services/models.py
+++ services/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class ServicePackage(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    base_price = models.DecimalField(max_digits=6, decimal_places=2)
+    number_of_images = models.PositiveIntegerField()
+    turnaround_days = models.PositiveIntegerField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
