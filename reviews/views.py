c9734fdebd5f73d9bec61501f13ecc7d2a018f0a
fila2021 <timtey2015@gmail.com>
2026-04-12 22:44:59 +0100

    Add full review views for product and custom request reviews

--- reviews/views.py
+++ reviews/views.py
@@ -1,30 +1,35 @@
-from django.contrib import messages
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404, redirect, render
-from django.urls import reverse
 
-from services.models import CustomRequest
 from shop.models import Product
+from services.models import CustomRequest
 
-from .forms import ReviewForm
 from .models import Review
+from .forms import ReviewForm
 
 
-def _redirect_target(review):
-    if review.product:
-        return reverse('product_detail', args=[review.product.slug])
-    if review.custom_request:
-        return reverse('request_detail', args=[review.custom_request.pk])
-    return '/'
+def review_list(request):
+    """
+    Display all reviews (latest first)
+    """
+    reviews = Review.objects.select_related(
+        'user',
+        'product',
+        'custom_request'
+    ).all()
+
+    context = {
+        'reviews': reviews
+    }
+    return render(request, 'reviews/review_list.html', context)
 
 
 @login_required
-def create_product_review(request, slug):
-    product = get_object_or_404(Product, slug=slug, is_active=True)
-    existing = Review.objects.filter(user=request.user, product=product).first()
-    if existing:
-        messages.info(request, 'You already reviewed this product. You can edit it instead.')
-        return redirect('edit_review', pk=existing.pk)
+def add_product_review(request, product_id):
+    """
+    Add a review for a specific product
+    """
+    product = get_object_or_404(Product, id=product_id)
 
     if request.method == 'POST':
         form = ReviewForm(request.POST)
@@ -33,25 +38,24 @@ def create_product_review(request, slug):
             review.user = request.user
             review.product = product
             review.save()
-            messages.success(request, 'Thanks for your review!')
-            return redirect('product_detail', slug=product.slug)
+            return redirect('product_detail', pk=product.id)
     else:
         form = ReviewForm()
 
-    return render(request, 'reviews/review_form.html', {
+    context = {
         'form': form,
         'target': product,
-        'title': f'Review {product.title}',
-    })
+        'type': 'product'
+    }
+    return render(request, 'reviews/add_review.html', context)
 
 
 @login_required
-def create_request_review(request, pk):
-    custom_request = get_object_or_404(CustomRequest, pk=pk, user=request.user)
-    existing = Review.objects.filter(user=request.user, custom_request=custom_request).first()
-    if existing:
-        messages.info(request, 'You already reviewed this request. You can edit it instead.')
-        return redirect('edit_review', pk=existing.pk)
+def add_custom_request_review(request, request_id):
+    """
+    Add a review for a custom request
+    """
+    custom_request = get_object_or_404(CustomRequest, id=request_id)
 
     if request.method == 'POST':
         form = ReviewForm(request.POST)
@@ -60,44 +64,13 @@ def create_request_review(request, pk):
             review.user = request.user
             review.custom_request = custom_request
             review.save()
-            messages.success(request, 'Thanks for your review!')
-            return redirect('request_detail', pk=custom_request.pk)
+            return redirect('request_detail', pk=custom_request.id)
     else:
         form = ReviewForm()
 
-    return render(request, 'reviews/review_form.html', {
+    context = {
         'form': form,
         'target': custom_request,
-        'title': f'Review request #{custom_request.pk}',
-    })
-
-
-@login_required
-def edit_review(request, pk):
-    review = get_object_or_404(Review, pk=pk, user=request.user)
-    if request.method == 'POST':
-        form = ReviewForm(request.POST, instance=review)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Review updated.')
-            return redirect(_redirect_target(review))
-    else:
-        form = ReviewForm(instance=review)
-
-    return render(request, 'reviews/review_form.html', {
-        'form': form,
-        'target': review.product or review.custom_request,
-        'title': 'Edit review',
-    })
-
-
-@login_required
-def delete_review(request, pk):
-    review = get_object_or_404(Review, pk=pk, user=request.user)
-    target_url = _redirect_target(review)
-    if request.method == 'POST':
-        review.delete()
-        messages.success(request, 'Review deleted.')
-        return redirect(target_url)
-
-    return render(request, 'reviews/review_confirm_delete.html', {'review': review})
+        'type': 'custom_request'
+    }
+    return render(request, 'reviews/add_review.html', context)
