07b0f033fd5cdd9818bac8a773a62c7178c11b64
fila2021 <timtey2015@gmail.com>
2026-04-13 05:13:09 +0100

    Add back navigation links to order detail page

---
+++ services/migrations/0005_merge_20260413_9999.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('services', '0004_alter_customrequest_options_and_more'),
+        ('services', '0004_servicepackage_image_url'),
+    ]
+
+    operations = [
+    ]
