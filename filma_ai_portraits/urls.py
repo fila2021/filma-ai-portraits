962df8a723b1344c0a1cfa2cc06b9273fae5351f
fila2021 <timtey2015@gmail.com>
2026-04-13 03:47:45 +0100

    Add AI Photos and Prompts as dedicated pages and nav links

--- filma_ai_portraits/urls.py
+++ filma_ai_portraits/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from home.views import home, browse
+from home.views import home, browse, ai_photos, ai_prompts
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', home, name='home'),
     path('browse/', browse, name='browse'),
+    path('ai-photos/', ai_photos, name='ai_photos'),
+    path('prompts/', ai_prompts, name='ai_prompts'),
 
     path('accounts/', include('accounts.urls')),
     path('account/', include('account.urls')),
